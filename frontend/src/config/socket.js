aa2fef805f4d193f82eb5e09ab425e609fdbbf0c
Mahetab Patel <149360481+petelmahetab@users.noreply.github.com>
2025-03-21 13:03:50 +0530

    Update socket.js

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -1,35 +1,25 @@
-import { io } from 'socket.io-client';
-
-let socketInstance;
-
 export const initializeSocket = (projectId) => {
     let apiUrl = import.meta.env.VITE_API_URL || 'https://chat-with-me-inky.vercel.app/';
-    
+    console.log('Connecting to:', apiUrl);
 
     if (socketInstance && socketInstance.connected) {
-     
+        console.log(`Reusing socket for project: ${projectId}`);
         return socketInstance;
     }
 
     const token = localStorage.getItem('token');
+    console.log('Token:', token);
+    console.log('Project ID:', projectId); 
     socketInstance = io(apiUrl, {
         auth: { token },
         query: { projectId },
-        transports: {'websocket'},
+        transports: ['websocket'],
+    });
+
+    socketInstance.on('connect', () => console.log('Socket connected successfully'));
+    socketInstance.on('connect_error', (err) => {
+        console.error('Connection failed:', err.message); // Log specific error
     });
 
     return socketInstance;
-};
-
-export const receiveMessage = (eventName, cb) => {
-    socketInstance.on(eventName, cb);
-}
-
-export const sendMessage = (eventName, data) => {
-    socketInstance.emit(eventName, data);
-}
-export const emitUserActivity = (action) => {
-    if (socketInstance) {
-        socketInstance.emit('user-activity', { action });
-    }
 };
