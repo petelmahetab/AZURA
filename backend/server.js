20b541e903865ee3bc998236fe5bd90b259aa7d3
Mahetab Patel <149360481+petelmahetab@users.noreply.github.com>
2025-03-21 12:21:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 4000;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://chat-with-me-inky.vercel.app/",
         methods: ["GET", "POST"],
         credentials: true,
     },
